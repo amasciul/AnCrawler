e028c811e7ff6e6db3f896e84686594a087e17e8
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2016-10-04 08:27:53 +0200

    Add print_json() func for debug purposes and wrap script in parse() func

--- parse.py
+++ parse.py
@@ -1,16 +1,22 @@
 import json
 
-with open("/Users/Alexandre/deputes.json") as data_file:
-    data = json.load(data_file)
+def parse():
+    with open("/Users/Alexandre/deputes.json") as data_file:
+        data = json.load(data_file)
 
-    for d in data["export"]["acteurs"]["acteur"]:
+        for d in data["export"]["acteurs"]["acteur"]:
 
-        lastname = d["etatCivil"]["ident"]["nom"]
-        firstname = d["etatCivil"]["ident"]["prenom"]
-        title = d["etatCivil"]["ident"]["civ"]
+            lastname = d["etatCivil"]["ident"]["nom"]
+            firstname = d["etatCivil"]["ident"]["prenom"]
+            title = d["etatCivil"]["ident"]["civ"]
 
-        mandat = d["mandats"]["mandat"]
-        if (len(mandat) > 0 and "election" in mandat[0].keys()):
-            department = mandat[0]["election"]["lieu"]["departement"]
+            mandat = d["mandats"]["mandat"]
+            if (len(mandat) > 0 and "election" in mandat[0].keys()):
+                department = mandat[0]["election"]["lieu"]["departement"]
 
-        print("%s %s %s %s" % (title, firstname, lastname, department))
+            print("%s %s %s %s" % (title, firstname, lastname, department))
+
+def print_json(data):
+    print(json.dumps(data))
+
+parse()
