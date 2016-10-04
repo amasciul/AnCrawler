404d11bdc14278757348db2cb66c46d4ef2081af
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2016-10-04 08:33:11 +0200

    Parse uid

--- parse.py
+++ parse.py
@@ -6,6 +6,8 @@ def parse():
 
         for d in data["export"]["acteurs"]["acteur"]:
 
+            uid = d["uid"]["#text"]
+
             lastname = d["etatCivil"]["ident"]["nom"]
             firstname = d["etatCivil"]["ident"]["prenom"]
             title = d["etatCivil"]["ident"]["civ"]
@@ -14,7 +16,7 @@ def parse():
             if (len(mandat) > 0 and "election" in mandat[0].keys()):
                 department = mandat[0]["election"]["lieu"]["departement"]
 
-            print("%s %s %s %s" % (title, firstname, lastname, department))
+            print("%s %s %s %s %s" % (uid, title, firstname, lastname, department))
 
 def print_json(data):
     print(json.dumps(data))
